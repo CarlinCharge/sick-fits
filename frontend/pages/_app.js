1a1e3cbdabd6d4608aee12f79569435093d182e0
CarlinCharge <sprintman210@gmail.com>
2021-03-07 11:59:18 -0500

    adds config for Apollo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,34 @@
+import { ApolloProvider } from '@apollo/client';
 import Router from 'next/router';
 
 // TODO: Swap in my own CSS
 import NProgress from 'nprogress';
+import withData from '../lib/withData';
 import Page from '../components/Page';
 import '../components/styles/nprogress.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
-export default function MyApp({ Component, pageProps }) {
+
+// eslint-disable-next-line react/prop-types
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
