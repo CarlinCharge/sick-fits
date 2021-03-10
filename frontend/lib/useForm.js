91e701a14eaf2c82905c20645b7a5eb5572949ce
CarlinCharge <sprintman210@gmail.com>
2021-03-09 19:05:39 -0500

    adds product form and hook

---
+++ frontend/lib/useForm.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  const [inputs, setInputs] = useState(initial);
+
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+    if (type === 'file') {
+      value[0] = e.target.files;
+    }
+    setInputs({
+      // gather our inputs
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
